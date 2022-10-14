6970b76a3b730ab5703e80c9bd8bb6221c9855ec
Marcos-Garcia-Medina <74043202+Marcos-Garcia-Medina@users.noreply.github.com>
2022-10-14 14:20:55 +0200

    Subido el proyecto

---
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -0,0 +1,46 @@
+package prZoologicoDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class AnimalDAO{
+	
+	private static Connection connection;
+	
+	public static void insertAnimal(Animal animal) {
+		try {
+			connection = openConnection();
+			String query = "insert into animales (nombre,peso_aproximado,habitat) values(?, ?, ?)";
+			
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			
+			preparedStatement.setString(1, animal.getNombre());
+			preparedStatement.setDouble(2, animal.getPeso_aproximado());
+			preparedStatement.setString(3, animal.getHabitat());
+			
+			preparedStatement.execute();
+			
+			closeConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static Connection openConnection() {
+		DatabaseConnection dbConnection = new DatabaseConnection();
+		connection = dbConnection.getConnection();
+		return connection;
+	}
+	
+	private static void closeConnection() {
+		try {
+			connection.close();
+			connection = null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -0,0 +1,12 @@
+package prZoologicoDAO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Animal a = new Animal("Perro","Casa", 30);
+		AnimalDAO.insertAnimal(a);
+
+	}
+
+}
