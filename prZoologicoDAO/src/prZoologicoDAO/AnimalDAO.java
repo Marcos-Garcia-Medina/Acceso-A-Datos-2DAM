82a784b11bb0996724417343f87e9d0ea3edba7e
Marcos-Garcia-Medina <74043202+Marcos-Garcia-Medina@users.noreply.github.com>
2022-10-21 12:19:15 +0200

    Delete hecho

--- prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -1,6 +1,7 @@
 package prZoologicoDAO;
 
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -22,6 +23,19 @@ public abstract class AnimalDAO{
 			preparedStatement.execute();
 			
 			closeConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void deleteAnimal() {
+		connection = openConnection();
+		String query = "delete from animales";
+		
+		try {
+			Statement statement = (Statement) connection.createStatement();
+			statement.executeUpdate(query);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- prZoologicoDAO/src/prZoologicoDAO/Main.java
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Animal a = new Animal("Perro","Casa", 30);
-		AnimalDAO.insertAnimal(a);
-
+		//Animal a = new Animal("Perro","Casa", 30);
+		//AnimalDAO.insertAnimal(a);
+		AnimalDAO.deleteAnimal();
 	}
 
 }
