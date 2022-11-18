7ef8f5a54a5f2c66295cb5e753b2b5cac3fa7b1d
Marcos-Garcia-Medina <74043202+Marcos-Garcia-Medina@users.noreply.github.com>
2022-11-18 12:36:57 +0100

    Clases Base creadas.

---
+++ prProyectoFinTrimestreMarcosGM/src/dao/InterfazDao.java
@@ -0,0 +1,36 @@
+package dao;
+
+import java.util.ArrayList;
+
+public interface InterfazDao<T> {
+	/**
+	 * Muestra todos los objetos T de la base de datos.
+	 * @return Un ArrayList de objetos t.
+	 */
+	public ArrayList<T> buscarTodos();
+	
+	/**
+	 * Muestra el objeto T con el id especificado.
+	 * @param i El id del objeto T.
+	 * @return Un objeto T.
+	 */
+	public T buscarPorId(int i);
+	
+	/**
+	 * Insertar el objeto T recibido en la base de datos.
+	 * @param t Un objeto T.
+	 */
+	public void insertar(T t);
+	
+	/**
+	 * Actualiza el objeto T de la base de datos.
+	 * @param t Un objeto T.
+	 */
+	public void modificar(T t);
+	
+	/**
+	 * Elimina el objeto T de la base de datos.
+	 * @param t Un objeto T.
+	 */
+	public void borrar(T t);
+}
